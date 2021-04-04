591825b6fc224128456908db0d685b656ca2e082
Hassan @Ubuntu <hassansalehe@gmail.com>
2021-04-04 18:56:20 +0200

    Add empty function to return current price

---
+++ hisaapi/__init__.py
@@ -0,0 +1,15 @@
+import investpy
+
+class HisaApi:
+    def get_current_price_euro(stock, country):
+        '''
+        Get current price of a stock in Euros.
+
+        Parameters
+        ----------
+        stock: str
+            The ticker of a stock
+        country: str
+            Country of the stock
+        '''
+       return 0
