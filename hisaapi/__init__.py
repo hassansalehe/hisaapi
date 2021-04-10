4eaaae90ad7299b8779c76f226f32e77d534a5ce
Hassan @Ubuntu <hassansalehe@gmail.com>
2021-04-11 00:03:50 +0200

    Round cents to 2 decimal places and do cleanup

--- hisaapi/__init__.py
+++ hisaapi/__init__.py
@@ -22,14 +22,10 @@ class HisaApi:
 
         data = investpy.get_stock_recent_data(stock=stock, country=country, order='descending', as_json=True)
         price = json.loads( data )['recent'][0]['close']
-        unit = self._get_currency(stock, country)
+        unit = json.loads( data )['recent'][0]['currency']
         if 'EUR' in unit:
             return price
 
         cross = investpy.get_currency_cross_recent_data(currency_cross='EUR/{}'.format(unit), order='descending', as_json=True)
         per_euro = json.loads( cross )['recent'][0]['close']
-        return price / float(per_euro)
-
-    def _get_currency(self, stock, country):
-        data = investpy.get_stock_recent_data(stock=stock, country=country, order='descending', as_json=True)
-        return json.loads( data )['recent'][0]['currency']
+        return round(price / float(per_euro), 2)
