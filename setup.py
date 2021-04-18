68357fd8d7c1fb7c982dbcd7f16b93c22e5e7869
Hassan @Ubuntu <hassansalehe@gmail.com>
2021-04-18 20:54:04 +0200

    Remove unrecognized classifiers

--- setup.py
+++ setup.py
@@ -19,10 +19,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
         "Topic :: Office/Business :: Financial",
-        "Topic :: Office/Business :: Financial :: Stocks",
-        "Topic :: Office/Business :: Financial :: Shares",
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
